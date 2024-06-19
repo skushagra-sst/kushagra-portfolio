520eb8351e008319df52a7a286adb20f0e908693
Kushagra S <kali@kali.teamkushagra.tech>
2024-06-19 10:12:22 +0530

    added resume

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -37,7 +37,7 @@ export default function NavBar() {
             </div>
             <div className='right'>
                 <div className="resumeBtn">
-                    Download Resume
+                    Download
                 </div>
             </div>
         </div>
